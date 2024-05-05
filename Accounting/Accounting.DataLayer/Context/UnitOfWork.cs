cb8951ceb9a7c15da32c61a2603e69db63bc74e2
Amir <amirhasantalebi1400@gmail.com>
2024-05-05 14:46:03 +0330

    Add Entitiy Model and Database, Create Repository and Context

---
+++ Accounting/Accounting.DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Services;
+
+namespace Accounting.DataLayer.Context
+{
+    public class UnitOfWork : IDisposable
+    {
+        MOAccountingEntities db = new MOAccountingEntities();
+
+        private GenericRepository<MobileModel> _mobileRepository;
+
+        public GenericRepository<MobileModel> MobileRepository
+        {
+            get
+            {
+                if (_mobileRepository == null)
+                {
+                    _mobileRepository = new GenericRepository<MobileModel>(db);
+                }
+
+                return _mobileRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
---
+++ Accounting/Accounting.DataLayer/Services/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace Accounting.DataLayer.Services
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        private MOAccountingEntities _db;
+        private DbSet<TEntity> _dbset;
+
+        public GenericRepository(MOAccountingEntities db)
+        {
+            _db = db;
+            _dbset = _db.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> where = null)
+        {
+            IQueryable<TEntity> query = _dbset;
+
+            if (where != null)
+            {
+                query = query.Where(where);
+            }
+
+            return query.ToList();
+        }
+
+        public virtual TEntity GetById(object Id)
+        {
+            return _dbset.Find(Id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            _dbset.Add(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbset.Attach(entity);
+            _db.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            if (_db.Entry(entity).State == EntityState.Detached) // هم میتونیم چک کنیم و هم مثل بالا تنها بنویسیم
+            {
+                _dbset.Attach(entity);
+            }
+            //_db.Entry(entity).State = EntityState.Deleted;
+            _dbset.Remove(entity);
+        }
+
+        public virtual void Delete(object Id)
+        {
+            var entity = GetById(Id);
+            Delete(entity);
+        }
+    }
+}
+}
